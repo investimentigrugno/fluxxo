7946476bf67cd25b52704201a317b893d89c6707
investimentigrugno <investimentigrugno@gmail.com>
2025-11-26 12:18:07 +0000

    A

--- python-service/app.py
+++ python-service/app.py
@@ -181,8 +181,8 @@ def get_ticker_info():
         
         ticker = yf.Ticker(ticker_input)
 
-        price = ticker_obj.history(period='1d')['Close'].iloc[-1]
-        currency = ticker.obj.info('currency')
+        price = ticker.history(period='1d')['Close'].iloc[-1]
+        currency = ticker.info('currency')
         
         if price is None:
             return jsonify({'error': f'Prezzo non disponibile per {ticker_input}'}), 404
